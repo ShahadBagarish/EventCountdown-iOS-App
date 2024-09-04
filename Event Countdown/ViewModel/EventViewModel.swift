99cb54c6e1c6f12cb4073c1d6a02d222854c0eab
Shahad Bagarish <Shahad.bagarish@gmail.com>
2024-09-04 10:27:32 +0300

    without ViewModel Manuplation

---
+++ Event Countdown/ViewModel/EventViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  observables.swift
+//  Event Countdown
+//
+//  Created by Shahad Bagarish on 03/09/2024.
+//
+
+import Foundation
+import Observation
+import SwiftUI
+
+@Observable class EventViewModel: Identifiable, Hashable{
+
+    var events: [Event] = []
+    
+    init(events: [Event]) {
+        self.events = events
+    }
+    
+    func append(){
+        let newEvent = Event(title: "Test", date: Date.now, textColor: Color.red)
+        events.append(newEvent)
+    }
+    
+    func remove(id: UUID){
+        events = events.filter { $0.id != id }
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    static func == (lhs: EventViewModel, rhs: EventViewModel) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+ 
